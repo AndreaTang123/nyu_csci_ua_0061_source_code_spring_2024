32f073974f22fa7d521052b9316172d5f5dad0d2
AndreaTang123 <157753288+AndreaTang123@users.noreply.github.com>
2024-01-30 16:33:53 -0500

    Update 01_Variables.js

--- days_03_04/intro_to_javascript_programming_challenges/01_Variables.js
+++ days_03_04/intro_to_javascript_programming_challenges/01_Variables.js
@@ -1,6 +1,9 @@
 // Programming Challenge:
 // Ask the user for their name
+let username = prompt("What is your name?");
 // Then have the website welcome the user using an <h1> tag
+document.write("<h1>Welcome! "+username+"</h1>");
 // Next, ask the user for a hex color code
+let hexcode = prompt("What hex color code you want?");
 // Use this code to create a div (give it a class of colorBox) on the screen with the correct color
-
+document.write("<div class="colorBox" style = "background-color:#${hexcode};"></div>);
